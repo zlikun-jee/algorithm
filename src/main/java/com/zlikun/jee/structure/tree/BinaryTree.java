ade86cdd06e8594dfc573073238c8c4c8b659c81
zlikun <zlikun-dev@hotmail.com>
2018-07-11 21:26:50 +0800

    二叉树的一些操作测试(未完)

--- src/main/java/com/zlikun/jee/structure/tree/BinaryTree.java
+++ src/main/java/com/zlikun/jee/structure/tree/BinaryTree.java
@@ -9,7 +9,7 @@ import lombok.Data;
  * @author zlikun <zlikun-dev@hotmail.com>
  * @date 2018/7/11 20:08
  */
-public class BinaryTree {
+public class BinaryTree<T> {
 
     /**
      * 节点类
@@ -70,21 +70,64 @@ public class BinaryTree {
         ]
         ------------------------------------------------------ */
 
+        BinaryTree<Integer> bt = new BinaryTree();
+
         // 前序遍历
         System.out.println("\n前序遍历");
         // 4	2	1	7	5	9
-        preOrder(root);
+        bt.preOrder(root);
 
         // 中序遍历
         System.out.println("\n中序遍历");
         // 1	2	4	5	7	9
-        inOrder(root);
+        bt.inOrder(root);
 
         // 后序遍历
         System.out.println("\n后序遍历");
         // 1	2	5	9	7	4
-        postOrder(root);
+        bt.postOrder(root);
 
+        // 下面的一些命题出自：https://www.jianshu.com/p/0190985635eb
+
+        // 求二叉树的最大深度(高度)：3
+        System.out.printf("\n二叉树的最大深度：%d%n", bt.maxDepth(root));
+
+        // 求二叉树中节点总数
+
+        // 求二叉树中叶子节点数
+
+        // 求二叉树中第n层(小于等于树高)节点数
+
+        // 判断二叉树是否平衡树
+
+        // 判断二叉树是否完全二叉树
+
+        // 判断两个二叉树是否完全相同
+
+        // 判断两个二叉树是否互为镜像
+
+        // 翻转二叉树 or 镜像二叉树
+
+        // 求两个二叉树的最低公共祖先节点
+
+        // 在二叉树中插入节点
+        // 在值为7的节点位置插入值为6的节点，原来的节点作为插入节点的右子节点
+        root.setRight(new TreeNode<>(6, null, root.getRight()));
+        // 使用中序遍历确认是否正确插入(由于是手动插入，未重排序，所以实际并不是有序的，这里只是为了验证插入正确)
+        // 1	2	4	6	5	7	9
+        // bt.inOrder(root);
+
+        // 打印二叉树中节点值之和等于给定值的所有路径
+
+        // 二叉树的搜索区间
+
+        // 二叉树的层次遍历
+
+        // 二叉树内两个节点的最长距离
+
+        // 计算 1 ... n 为节点的二叉树有多少种
+
+        // 判断二叉树是否合法的二叉搜索树(BST)
 
         System.out.println();
     }
@@ -93,9 +136,8 @@ public class BinaryTree {
      * 前序遍历：DLR
      *
      * @param node
-     * @param <T>
      */
-    private static <T> void preOrder(TreeNode<T> node) {
+    private void preOrder(TreeNode<T> node) {
         if (node == null) {
             return;
         }
@@ -108,9 +150,8 @@ public class BinaryTree {
      * 中序遍历：LDR
      *
      * @param node
-     * @param <T>
      */
-    private static <T> void inOrder(TreeNode<T> node) {
+    private void inOrder(TreeNode<T> node) {
         if (node == null) {
             return;
         }
@@ -123,15 +164,30 @@ public class BinaryTree {
      * 后序遍历：LRD
      *
      * @param node
-     * @param <T>
      */
-    private static <T> void postOrder(TreeNode<T> node) {
+    private void postOrder(TreeNode<T> node) {
         if (node == null) {
             return;
         }
         postOrder(node.getLeft());
         postOrder(node.getRight());
         System.out.print(node.getData() + "\t");
+    }
+
+    /**
+     * 求给定节点的最大深度
+     *
+     * @param node
+     * @return
+     */
+    private int maxDepth(TreeNode<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(node.getLeft());
+        int rightDepth = maxDepth(node.getRight());
+        // 取左子节点深度与右子节点深度间中的一个
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 
 }
