0e3234c9d56113780bd526d024b31fa2f06309e8
zlikun <zlikun-dev@hotmail.com>
2018-07-24 11:24:48 +0800

    插入排序记录遍历次数

--- src/main/java/com/zlikun/jee/sort/Inserting.java
+++ src/main/java/com/zlikun/jee/sort/Inserting.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 插入排序
+ *
  * @author zlikun
  * @date 2018/7/3 16:19
  */
@@ -12,24 +13,15 @@ public class Inserting {
     public static void main(String[] args) {
 
         // 排序前的数组
-        int[] array = {27, 12, 36, 12, 24, 68, 59, 91, 45};
+        int[] array = {27, 12, 36, 12, 24, 45, 68, 59, 91};
 
         // 打印排序前的数组
         // [27, 12, 36, 12, 24, 68, 59, 91, 45]
         System.out.println(Arrays.toString(array));
 
         // 执行排序
-        sort(array);
-
-        // 打印排序后的结果
-        // [12, 12, 24, 27, 36, 45, 59, 68, 91]
-        System.out.println(Arrays.toString(array));
-
-    }
-
-    private static final void sort(int[] array) {
-
-        for (int i = 1; i < array.length ; i++) {
+        int times = 0;
+        for (int i = 1; i < array.length; i++) {
 
             // 取第i个元素作为哨兵，其前面的元素则为有序列表
             // 当i == 1的时候，前面只有一个元素，所以一定是有序列表
@@ -39,8 +31,9 @@ public class Inserting {
             // 当遇到比哨兵大的元素时，将该元素后的所有元素向后移动一次
             // 将将哨兵插入到移动元素后空出来的位置上
             // 因为需要向后移动元素，所以遍历时，需要倒序遍历
-            for (int j = i - 1; j >= 0 ; j--) {
+            for (int j = i - 1; j >= 0; j--) {
 
+                times++;
                 if (array[j] > sentinel) {
                     array[j + 1] = array[j];
                     array[j] = sentinel;
@@ -53,7 +46,12 @@ public class Inserting {
 
         }
 
-    }
+        System.out.println(String.format("长度为 %d 的序列，冒泡排序一共比较了 %d 次", array.length, times));
 
+        // 打印排序后的结果
+        // [12, 12, 24, 27, 36, 45, 59, 68, 91]
+        System.out.println(Arrays.toString(array));
+
+    }
 
 }
